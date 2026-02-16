9e8025113951f70f3f55a23b527620741d46ed13
pand4monium <229127319+pand4monium@users.noreply.github.com>
2026-02-16 21:57:56 +0800

    Added functionality to todoItem class

---
+++ src/class/todoItem.js
@@ -0,0 +1,19 @@
+class todoItem {
+
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = new Date(dueDate.year, dueDate.month, dueDate.day, 0, 0);
+        this.priority = priority;
+        this.notes = "";
+        this.status = 0;
+    }
+
+    changeStatus() {
+        if (this.status === 0) {
+            this.status = 1;        
+        } else {
+            this.status = 0;
+        }
+    }
+}
